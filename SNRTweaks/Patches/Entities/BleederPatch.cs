e7b6e2dabcd001554deea81527dfa6acaaaa44b7
RosarianGX <rosegx8799@gmail.com>
2025-02-25 12:39:36 +0000

    Implemented Bleeder Removal

---
+++ SNRTweaks/Patches/Entities/BleederPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using Nautilus.Extensions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SNRTweaks.Patches.Entities
+{
+    [HarmonyPatch]
+    public class BleederPatch
+    {
+        [HarmonyPatch(typeof(Bleeder), nameof(Bleeder.Start)), HarmonyPrefix]
+        private static void BleederStart_Prefix(Bleeder __instance)
+        {
+            if (Plugin.Options.areBleedersDisabled)
+            {
+                GameObject.Destroy(__instance.gameObject);
+                Plugin.Logger.LogInfo("Bleeder was destroyed");
+            }
+        }
+    }
+}
