4e7693ad0636b6174d6318359c7a79abef1c15b4
RosarianGX <rosegx8799@gmail.com>
2025-02-25 13:11:39 +0000

    Removed .Equals from wasSeamothSliderChanged check

---
+++ SNRTweaks/Patches/Vehicles/SeamothSpeedPatch.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace SNRTweaks.Patches.Vehicles
+{
+    [HarmonyPatch]
+    public class SeamothSpeedPatch
+    {
+        internal static float defaultForwardForce;
+
+        [HarmonyPatch(typeof(SeaMoth), nameof(SeaMoth.Awake)), HarmonyPostfix]
+        private static void SeamothAwake_PostFix(SeaMoth __instance) 
+        {
+            defaultForwardForce = __instance.forwardForce;
+
+            __instance.forwardForce *= Plugin.Options.seamothSpeedMultiplier;
+        }
+
+        [HarmonyPatch(typeof(SeaMoth), nameof(SeaMoth.Update)), HarmonyPostfix]
+        private static void SeamothUpdate_PostFix()
+        {
+            if (Plugin.Options.wasSeamothSliderChanged) 
+            {
+                foreach (var seamoths in Object.FindObjectsOfType<SeaMoth>())
+                {
+                    seamoths.forwardForce = defaultForwardForce;
+
+                    seamoths.forwardForce *= Plugin.Options.seamothSpeedMultiplier;
+                    Plugin.Options.wasSeamothSliderChanged = false;
+                }
+            }
+        }
+    }
+}
