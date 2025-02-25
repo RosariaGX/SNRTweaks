ae136ad2b75e4e954a100aa6600e45bf1d40408e
RosarianGX <rosegx8799@gmail.com>
2025-02-25 13:02:05 +0000

    Changed to only multiply ForwardForce & removed debug logging and unused imports

--- SNRTweaks/Patches/Utility/SeamothSpeedPatch.cs
+++ SNRTweaks/Patches/Utility/SeamothSpeedPatch.cs
@@ -7,22 +7,13 @@ namespace SNRTweaks.Patches.Utility
     public class SeamothSpeedPatch
     {
         internal static float defaultForwardForce;
-        internal static float defaultBackwardsForce;
-        internal static float defaultSidewaysForce;
-        internal static float defaultVerticalForce;
 
         [HarmonyPatch(typeof(SeaMoth), nameof(SeaMoth.Awake)), HarmonyPostfix]
         private static void SeamothAwake_PostFix(SeaMoth __instance) 
         {
             defaultForwardForce = __instance.forwardForce;
-            defaultBackwardsForce = __instance.backwardForce;
-            defaultSidewaysForce = __instance.sidewardForce;
-            defaultVerticalForce = __instance.verticalForce;
 
             __instance.forwardForce *= Plugin.Options.seamothSpeedMultiplier;
-            __instance.backwardForce *= Plugin.Options.seamothSpeedMultiplier;
-            __instance.sidewardForce *= Plugin.Options.seamothSpeedMultiplier;
-            __instance.verticalForce *= Plugin.Options.seamothSpeedMultiplier;
         }
 
         [HarmonyPatch(typeof(SeaMoth), nameof(SeaMoth.Update)), HarmonyPostfix]
@@ -32,24 +23,12 @@ namespace SNRTweaks.Patches.Utility
             {
                 foreach (var seamoths in GameObject.FindObjectsOfType<SeaMoth>())
                 {
-                    ResetSeamothValues(seamoths);
+                    seamoths.forwardForce = defaultForwardForce;
 
                     seamoths.forwardForce *= Plugin.Options.seamothSpeedMultiplier;
-                    seamoths.backwardForce *= Plugin.Options.seamothSpeedMultiplier;
-                    seamoths.sidewardForce *= Plugin.Options.seamothSpeedMultiplier;
-                    seamoths.verticalForce *= Plugin.Options.seamothSpeedMultiplier;
-                    Plugin.Logger.LogInfo($"Updated Seamoth Speed: {Plugin.Options.seamothSpeedMultiplier}");
                     Plugin.Options.wasSeamothSliderChanged = false;
                 }
             }
-        }
-
-        private static void ResetSeamothValues(SeaMoth __instance)
-        {
-            __instance.forwardForce = defaultForwardForce;
-            __instance.backwardForce = defaultBackwardsForce;
-            __instance.sidewardForce = defaultSidewaysForce;
-            __instance.verticalForce = defaultVerticalForce;
         }
     }
 }
